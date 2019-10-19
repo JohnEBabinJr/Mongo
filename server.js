2f8a43708c65551074fd2e574d72a28ea461ea9e
John Babin <johnebabinjr@gmail.com>
2019-10-19 13:07:08 -0700

    added connection to mongo

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
-
+//Set up mongo
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscraper";
 mongoose.connect(MONGODB_URI);
 
